eaf50f2e35c2bd26c13dcffcce5c4f0d65016912
Seth Watson <seth.watson1@gmail.com>
2018-11-20 00:45:05 -0700

    added StatsScreenVC

---
+++ EarthLove/StatsScreenViewController.swift
@@ -0,0 +1,20 @@
+//
+//  StatsScreenViewController.swift
+//  EarthLove
+//
+//  Created by Seth Watson on 11/20/18.
+//  Copyright © 2018 Seth Watson. All rights reserved.
+//
+
+import UIKit
+
+class StatsScreenViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
+
+
