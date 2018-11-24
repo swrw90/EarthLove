4f2204fc0a577369fd5b0c982d80408564bb2aa8
Seth Watson <seth.watson1@gmail.com>
2018-11-23 23:59:00 -0700

    added search bar

--- EarthLove/HistoryViewController.swift
+++ EarthLove/HistoryViewController.swift
@@ -12,9 +12,18 @@ class HistoryViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.contentInset = UIEdgeInsets(top: 64, left: 0, bottom: 0, right: 0)
     }
 
+    //MARK: - Outlets
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
 
 }
 
+extension HistoryViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        print("The search text is: '\(searchBar.text!)'")
+    }
+}
