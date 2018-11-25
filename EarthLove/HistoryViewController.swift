7bc75dc2fa082a4f8a3b7178accaa0d6020719d6
Seth Watson <seth.watson1@gmail.com>
2018-11-24 23:24:12 -0700

    added tableView data source extension

--- EarthLove/HistoryViewController.swift
+++ EarthLove/HistoryViewController.swift
@@ -9,21 +9,38 @@
 import UIKit
 
 class HistoryViewController: UIViewController {
-
+    
     //MARK: - Outlets
-    @IBOutlet weak var tableView: UITableView!
+
     @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        tableView.contentInset = UIEdgeInsets(top: 64, left: 0, bottom: 0, right: 0)
+tableView.contentInset = UIEdgeInsets(top: 64, left: 0, bottom: 0, right: 0)
+    }
+}
+
+
+//MARK: - TableView Data Source
+
+extension HistoryViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
     }
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
 }
+
+
+//MARK: - SearchBarDelegate
 
 extension HistoryViewController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         print("The search text is: '\(searchBar.text!)'")
     }
+    
 }
