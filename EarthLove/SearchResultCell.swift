065b4cdb7fe8db8f37ea4df1ae16e41ff83df313
Seth Watson <seth.watson1@gmail.com>
2018-11-25 23:52:18 -0700

    changed cell selection color

--- EarthLove/SearchResultCell.swift
+++ EarthLove/SearchResultCell.swift
@@ -13,16 +13,18 @@ class SearchResultCell: UITableViewCell {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var subTitleLabel: UILabel!
     @IBOutlet weak var categoryImageView: UIImageView!
-
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        let selectedView = UIView(frame: CGRect.zero)
+        selectedView.backgroundColor = UIColor(red: 20/255, green: 140/255, blue: 60/255, alpha: 0.5)
+        selectedBackgroundView = selectedView
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
-
+    
 }
