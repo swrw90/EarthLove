0139d69564ac6a57d2739fb88fbef50591c414da
Seth Watson <seth.watson1@gmail.com>
2018-12-06 18:40:43 -0700

    added methods to Challenge CoreData Class to fetch random challenge from context by fetchOffset

--- Challenge+CoreDataClass.swift
+++ Challenge+CoreDataClass.swift
@@ -15,6 +15,7 @@ typealias JSON = [String: Any]
 @objc(Challenge)
 public class Challenge: NSManagedObject {
     
+    
     class func insertToStore(from json: [JSON], in context: NSManagedObjectContext)  {
         _ = json.compactMap { challenge(from: $0, in: context) }
         
@@ -24,13 +25,13 @@ public class Challenge: NSManagedObject {
             fatalError("Failed to save")
         }
     }
-        
+    
     class func challenge(from json: JSON, in context: NSManagedObjectContext) -> Challenge? {
         // Make sure these properties are coming from the json. If it's not Challenge cannot exist
         // so return nil.
         guard let identifier = json["identifier"] as? Int64,
             let isCompleted = json["isCompleted"] as? Bool else { return nil }
-
+        
         // fetch the challenge with identifier, if it doesn't exist create a new one.
         let challenge = fetch(with: identifier, in: context) ?? Challenge(context: context)
         
@@ -64,17 +65,42 @@ public class Challenge: NSManagedObject {
         }
     }
     
-    /// Setup fetchRequest
-    let request: NSFetchRequest<Challenge> = Challenge.fetchRequest()
-    
-    var fetchRequest:NSFetchRequest<Challenge> = {
-        let request = NSFetchRequest<Challenge>(entityName: Challenge)
+    static var incompleteChallengeFetchRequest: NSFetchRequest<Challenge> {
+        // Create an instance of NSFetchRequest of Challenge type
+        let request: NSFetchRequest<Challenge> = Challenge.fetchRequest()
+        
+        // Format fetchRequest to fetch Challenges with property isCompleted set to false
+        request.predicate = NSPredicate(format: "%K == NO", #keyPath(Challenge.isCompleted))
+        
+        // Use sortDescriptors to define the order of response from the fetchRequest, no order necessary, set to empty array
+        request.sortDescriptors = []
+        
+        return request
     }
     
-    /// Setup NSPredicate & sortDescriptor
-    let predicate = NSPredicate(format: "%K == NO", argumentArray: [#keyPath(Challenge.isCompleted), "false"])
-    let sortDescriptor = NSSortDescriptor(key: "fetchOffset", ascending: true)
-//    fetchRequest.sortDescriptor = [sortDescriptor]
+    class func getContextCount(in context: NSManagedObjectContext) -> Int {
+        do {
+            return try context.count(for: incompleteChallengeFetchRequest)
+        } catch {
+            print("error")
+            return 0
+        }
+    }
+    
+    class func createChallengeFetchOffset(with count: Int, using fetchRequest: NSFetchRequest<Challenge>) -> Int {
+        // Get random number ranging from 0 and mangagedObjectContext count
+        let randomNumber = Int.random(in: 0 ..< count)
+        
+        // Set random number as fetchOffset property for fetchRequest
+        fetchRequest.fetchOffset = randomNumber
+        return fetchRequest.fetchOffset
+    }
+    
+    class func getRandomIncompleteChallenge(in context: NSManagedObjectContext, with identifier: Int64) -> Challenge? {
+        if let challenge = fetch(with: identifier, in: context) {
+            return challenge
+        } else {
+            return nil
+        }
+    }
 }
-
-
