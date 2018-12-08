77098ca546ca241c4d93b568f055bceb3ceaebce
Seth Watson <seth.watson1@gmail.com>
2018-12-08 01:17:46 -0700

    initial commit, modified viewDidLoad, began skip logic

--- EarthLove/ChallengeViewController.swift
+++ EarthLove/ChallengeViewController.swift
@@ -12,6 +12,7 @@ import CoreData
 class ChallengeViewController: UIViewController {
     
     var managedObjectContext: NSManagedObjectContext?
+    var skipCount = 0
     
     // MARK: - Outlets
     
@@ -28,17 +29,22 @@ class ChallengeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        displayNewChallenge()
+    }
+    
+    
+    private func displayNewChallenge() {
         if let context = managedObjectContext {
             
             if let fetchRequest = Challenge.createRandomChallengeFetchRequest(with: context) {
                 
                 if let challenge = Challenge.fetchRandomChallenge(with: fetchRequest, in: context) {
-//                    print(challenge)
                     setupChallengeUI(with: challenge)
                 }
             }
         }
     }
+    
     
     
     func setupChallengeUI(with challenge: Challenge) {
@@ -52,13 +58,23 @@ class ChallengeViewController: UIViewController {
     
     //MARK: - Actions
     
-    @IBAction func skipButton(_ sender: Any) {
-        // if clicked dismiss current challenge object and update UI to display new challenge object.
-        // if skip is clicked 3 times any addition clicks will trigger HUD informing there are no more skips allowed for 24 hours.
-        // after 24 hours reset number of allowed skips to 3
+    // if clicked dismiss current challenge object and update UI to display new challenge object.
+    // if skip is clicked 3 times any addition clicks will trigger HUD informing there are no more skips allowed for 24 hours.
+    // after 24 hours reset number of allowed skips to 3
+    @IBAction func skipButton(_ sender: Any ) {
+        displayNewChallenge()
+        skipCount += 1
+        
+        if skipCount == 3 {
+            print("3 skips")
+        }
+        
+        
     }
     
     @IBAction func completeButton(_ sender: Any) {
+        
+        print("called")
         // if clicked trigger HUD celebrating Challenge completion.
         // change Challenge.isCompleted to true until all challenges are complete or 1 year passes.
         // display pending challenge view for 24 hours informing user must wait 24 hours for a new challenge and that they will be notified
