a8160a9e1cceef338141cc2d4896eb43c530c585
Seth Watson <seth.watson1@gmail.com>
2018-12-28 19:16:32 -0700

    Removed old commented out code, added comments.

--- EarthLove/HistoryViewController.swift
+++ EarthLove/HistoryViewController.swift
@@ -12,47 +12,54 @@ import CoreData
 private let searchResultCell = "SearchResultCell"
 private let nothingFoundCell = "NothingFoundCell"
 
+/// MARK: - Displays the history of users completed challenges.
 class HistoryViewController: UIViewController {
     
     
     // MARK: - Properties
 
     var managedObjectContext: NSManagedObjectContext?
-//    var challenges = [Challenge]() {
-//        didSet {
-//            tableView.reloadData()
-//        }
-//    }
+    
     
     //MARK: - Outlets
     
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
     
+    
     // MARK: - NSFetchedResultsController
     
+    // Set delegate for any old versions of fetchedResultsController to nil to prevent memory leaks.
     private var _fetchedResultsController: NSFetchedResultsController<Challenge>? {
         didSet {
             oldValue?.delegate = nil
         }
     }
     
+    /// Check if fetchedResultsController already exists, if so return it, if not create a new instance of fetchedResultsController and set it to _fetchedResultsController.
     private var fetchedResultsController: NSFetchedResultsController<Challenge> {
         guard let context = managedObjectContext else { fatalError("Couldn't find context.") }
+        
+        // Check if fetchedResultsController already exists.
         if let fetchedResultsController = _fetchedResultsController { return fetchedResultsController }
         
         var fetchedResultsController: NSFetchedResultsController<Challenge>
         
+        // TODO: - Move to core data class as a func
+        // Create a fetchRequest for completed challenges.
         let fetchRequest: NSFetchRequest<Challenge> = Challenge.fetchRequest()
         fetchRequest.predicate = Challenge.isCompletedPredicate
         fetchRequest.sortDescriptors = [] // Support ordering by completion date
         
+        // Create an instance of NSFetchedResultsController using fetchRequest and context.
         fetchedResultsController = NSFetchedResultsController<Challenge>(fetchRequest: fetchRequest, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: nil)
         fetchedResultsController.delegate = self
         
+        // Set value of _fetchedResultsController.
         _fetchedResultsController = fetchedResultsController
         
         do {
+            // performFetch can throw, try performFetch and catch any errors.
             try fetchedResultsController.performFetch()
         } catch {
             fatalError("Failed to fetch data \(error)")
@@ -73,8 +80,6 @@ class HistoryViewController: UIViewController {
         cellNib = UINib(nibName: nothingFoundCell, bundle: nil)
         tableView.register(cellNib, forCellReuseIdentifier: nothingFoundCell)
 
-        
-//        challenges = getCompletedChallenges()
     }
     
     private func getCompletedChallenges() -> [Challenge] {
@@ -88,29 +93,15 @@ class HistoryViewController: UIViewController {
 extension HistoryViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        if challenges.count == 0 {
-//            return 1
-//        } else {
-//            return challenges.count
-//        }
         
-        return fetchedResultsController.fetchedObjects?.count ?? 0
+        return fetchedResultsController.fetchedObjects?.count ?? 1
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-//        if challenges.count == 0 {
-//            return tableView.dequeueReusableCell(withIdentifier: nothingFoundCell, for: indexPath)
-//        } else {
-//            let cell = tableView.dequeueReusableCell(withIdentifier: searchResultCell, for: indexPath) as! SearchResultCell
-//
-//            let challenge = challenges[indexPath.row]
-//            cell.titleLabel.text = challenge.title
-//            cell.subTitleLabel.text = challenge.category
-//            return cell
-//        }
         guard let cell = tableView.dequeueReusableCell(withIdentifier: searchResultCell, for: indexPath) as? SearchResultCell else { return UITableViewCell() }
         
+        // Assign at the indexPath to challenge constant to be displayed in the returned cell.
         let challenge = fetchedResultsController.object(at: indexPath)
         cell.titleLabel.text = challenge.title
         cell.subTitleLabel.text = challenge.category
