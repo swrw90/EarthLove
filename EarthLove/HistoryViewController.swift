6ebc716b2f55057e61d4e3b36691e3722dba1d87
Seth Watson <seth.watson1@gmail.com>
2019-02-07 00:40:30 -0700

    filtered history view list by category

--- EarthLove/HistoryViewController.swift
+++ EarthLove/HistoryViewController.swift
@@ -28,7 +28,7 @@ class HistoryViewController: UIViewController {
     var selectedCategory: Category?
     
     // MARK: - Outlets
-
+    
     @IBOutlet weak var tableView: UITableView!
     
     
@@ -49,9 +49,10 @@ class HistoryViewController: UIViewController {
         if let fetchedResultsController = _fetchedResultsController { return fetchedResultsController }
         
         var fetchedResultsController: NSFetchedResultsController<Challenge>
-        let fetchRequest = Challenge.completedChallengesFetchRequest()
+        let fetchRequest = Challenge.completedChallengesFetchRequest(category: selectedCategory)
         
         // 8. set fetchRequest predicate to filter out everything but selected category. Only do this IF selectedCategory exists.
+
         
         // Create an instance of NSFetchedResultsController using fetchRequest and context.
         fetchedResultsController = NSFetchedResultsController<Challenge>(fetchRequest: fetchRequest, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: nil)
@@ -93,25 +94,15 @@ class HistoryViewController: UIViewController {
     
     @IBAction func displayCategoriesMenu(_ sender: Any) {
         
-        // Creates an instance of CategoriesMenuViewController
         guard let categoriesMenuVC: CategoriesMenuViewController = self.storyboard!.instantiateViewController(withIdentifier: categoriesMenuIdentifier) as? CategoriesMenuViewController else { return }
         
-        // 2.5 set delegate on CateogriesMenuViewController to self.
         categoriesMenuVC.delegate = self
-        
-        // Defines the constraints of the overlay view, using IB auto-constraints.
         categoriesMenuVC.view.frame = self.view.bounds;
-        
-        // Called by addChild just before adding categoriesVC over top historyVC to prepare.
         categoriesMenuVC.willMove(toParent: self)
         
-        // Adds categoriesMenuVC to top level of view hierchy list.
         self.view.addSubview(categoriesMenuVC.view)
-        
-        // Adds categoriesMenuVC as a child to historyVC
         self.addChild(categoriesMenuVC)
         
-        // Called after categoriesVc has moved as an overlay to historyVC.
         categoriesMenuVC.didMove(toParent: self)
         
     }
@@ -160,29 +151,23 @@ extension HistoryViewController: UITableViewDelegate, UITableViewDataSource {
     
 }
 
+// MARK: - FecthedResultsControllerDelegate
 
-//MARK: - SearchBarDelegate
-
-
-/// Reloads tableView if HistoryVC content has changed. 
+/// Reloads tableView if HistoryVC content has changed.
 extension HistoryViewController: NSFetchedResultsControllerDelegate {
     func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
         tableView.reloadData()
     }
 }
 
-
-
-// 4. Create an extension of HistoryViewController to conform to protocol you created and
-//    conform to the protocl by adding the protocol method that passes in the category.
-
 extension HistoryViewController: CategoriesMenuViewControllerDelegate {
-   
+    
     func handleSelectedCategory(category: Category) {
-       
-        print(category)
-        
-        // 6. set the selected category to the one that was selected from CategoriesVC that was passed into here by delegation pattern
+
         selectedCategory = category
+        _fetchedResultsController = nil
+        
+        self.tableView.reloadData()
+        
     }
 }
